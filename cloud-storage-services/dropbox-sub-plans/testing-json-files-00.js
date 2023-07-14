62e0516b23984fb2bf9416cbb685b92106ca82d5
Laurence2013 <lozza244@protonmail.com>
2023-07-14 13:35:51 +0100

    01commit

---
+++ cloud-storage-services/dropbox-sub-plans/testing-json-files-00.js
@@ -0,0 +1,11 @@
+const dropbox = require("./dropbox-sub-plans.json");
+
+const dropbox_00 = dropbox[0];
+
+console.log(dropbox_00.dropbox_00);
+console.log(dropbox_00.dropbox_00.bard_prompt_00);
+
+console.log(dropbox_00.dropbox_01);
+console.log(dropbox_00.dropbox_01.monthly_plan_00);
+
+console.log(dropbox_00.dropbox_02);
